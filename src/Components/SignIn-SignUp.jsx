f56a9290d1d4a05238a30494fd268c9baa95e573
Aditya-Karki03 <adityakarki201@gmail.com>
2024-04-28 19:30:48 +0530

    Toggle functionality

--- src/Components/SignIn-SignUp.jsx
+++ src/Components/SignIn-SignUp.jsx
@@ -11,15 +11,34 @@ export default function SignInAndUp(){
 
     return(
         <div className="w-96 h-3/5 rounded-lg flex flex-col gap-5 justify-center items-center bg-[#DDE6ED]">
-            <h2 className="font-semibold text-lg">Welcome To Chat Application!</h2>
+            {
+                signIn?
+                <h2 className="font-semibold text-lg">Please do sign Up!</h2>:
+                <h2 className="font-semibold text-lg">Welcome To Chat Application!</h2>
+            }
+            
             <div className="w-4/5 h-12">
                 <input type="text" className="w-full h-full rounded-md px-2 outline-none" placeholder="Name" />
             </div>
+            {
+                signIn && <>
+                    <div className="w-4/5 h-12">
+                        <input type="text" className="w-full h-full rounded-md px-2 outline-none" placeholder="Username" />
+                    </div>
+                    <div className="w-4/5 h-12">
+                        <input type="text" className="w-full h-full rounded-md px-2 outline-none" placeholder="Email" />
+                    </div>
+                </>
+            }
             <div className="w-4/5 h-12">
                 <input type="password" className="w-full h-full rounded-md px-2 outline-none" placeholder="Password" />
             </div>
-            <button className="h-12 rounded-md bg-[#27374D] w-4/5 text-[#DDE6ED] hover:bg-[black]">Login</button>
-            <p>Do not have the account click<span className="font-semibold cursor-pointer " onClick={handleToggle}> Here</span></p>
+            <button className="h-12 rounded-md bg-[#27374D] w-4/5 text-[#DDE6ED] hover:bg-[black]">{
+                signIn?'Sign-Up':'Sign-In'
+            }</button>
+            <p>{
+                    signIn?'Have an account click':'Do not have the account click'
+                }<span className="font-semibold cursor-pointer " onClick={handleToggle}> Here</span></p>
         </div>
     )
 }
